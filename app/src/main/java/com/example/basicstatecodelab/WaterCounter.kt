6bdc53b91f16b9f8dfc1e85e3c90b4bf3cead713
Evan Greer <evan.greer@evan.greer>
2025-03-03 12:37:52 -0700

    adds state memory

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -5,19 +5,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text("You've had $count glasses.", Modifier.padding(top = 32.dp))
+    Column(modifier = modifier.padding(32.dp)) {
+        var count by remember { mutableIntStateOf(0) }
+
+        Text("You've had $count glasses.")
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
-
         }
     }
 }
